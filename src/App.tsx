f73b3d4521f37b04d8a67dd53a467e316655a851
joey iny <joeyainy@gmail.com>
2023-09-23 12:47:53 -0400

    Set up EOA with particle

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ParticleAuthModule, ParticleProvider } from "@biconomy/particle-auth";
+import { ethers } from "ethers";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const particle = new ParticleAuthModule.ParticleNetwork({
+    projectId: "af0a7a24-5167-41ae-b85d-e4f70503553f",
+    clientKey: "cm3nVwaX5JFjjtzwacZAGof9Op4oB34X8g3BzN8b",
+    appId: "1abd1aaf-810f-43df-8469-960edf59d8e1",
+    wallet: {
+      displayWalletEntry: true,
+      defaultWalletEntryPosition: ParticleAuthModule.WalletEntryPosition.BR,
+    },
+  });
+  const connect = async () => {
+    try {
+      const userInfo = await particle.auth.login();
+      console.log("Logged in user:", userInfo);
+      const particleProvider = new ParticleProvider(particle.auth);
+      console.log({ particleProvider });
+      const web3Provider = new ethers.providers.Web3Provider(particleProvider, "any");
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <button onClick={connect}> Connect </button>
+    </main>
+  );
 }
 
-export default App
+export default App;
