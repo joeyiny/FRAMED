8b0f1b484920e617e0eca69e658d18265cef8737
joey iny <joeyainy@gmail.com>
2023-09-24 03:05:27 -0400

    Create enums for state

--- src/screens/in-game.tsx
+++ src/screens/in-game.tsx
@@ -3,17 +3,23 @@ import Navbar from "@/components/navbar";
 import TutorialFlow from "./tutorial-flow";
 import { useEffect, useState } from "react";
 import WaitingRoom from "./waiting-room";
-import { CONTRACT_ADDRESS } from "./waiting-room";
+// import { CONTRACT_ADDRESS } from "./waiting-room";
 import { getGameStateFromContract } from "@/lib/game-functions";
+import { ClientState, GamePhase } from "@/types";
 
 const InGameScreen = () => {
-  const [gamePhase, setGamePhase] = useState<"tutorial" | "inRoom">("tutorial");
+  const [clientState, setClientState] = useState<ClientState>(ClientState.Tutorial);
+  const [gamePhase, setGamePhase] = useState<GamePhase>(GamePhase.WaitingForPlayers);
   useEffect(() => {
     const fetchGameState = async () => {
       try {
         const r = await getGameStateFromContract();
-        console.log(r);
-        return r;
+        if (!r) {
+          throw Error("There was an issue getting the game state from the contract.");
+        }
+        if (r === 0) {
+          setGamePhase(GamePhase.WaitingForPlayers);
+        }
       } catch (err) {
         console.error(err);
       }
@@ -23,8 +29,8 @@ const InGameScreen = () => {
   return (
     <div>
       <Navbar />
-      {gamePhase === "tutorial" && <TutorialFlow setGamePhase={setGamePhase} />}
-      {gamePhase === "inRoom" && <WaitingRoom />}
+      {clientState === "tutorial" && <TutorialFlow setClientState={setClientState} />}
+      {clientState === "inGame" && <WaitingRoom />}
     </div>
   );
 };
---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export enum ClientState {
+  Tutorial = "tutorial",
+  InGame = "inGame",
+}
+
+export enum GamePhase {
+  WaitingForPlayers = "waitingForPlayers",
+  AwaitPlayerActions = "awaitPlayerActions",
+  Voting = "voting",
+  Results = "results",
+}
