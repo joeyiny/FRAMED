16c50ea76c7659e53731b6fb2d0754911837e4fa
remi <remigai@bu.edu>
2023-09-24 00:09:01 -0400

    added fhevmjs

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,41 @@
 import "./App.css";
 
 // import InGameScreen from "./screens/in-game";
+import { useState, useEffect } from "react";
+import { init, getInstance } from "./lib/fhevm";
 import { Button } from "./components/ui/button";
 import WaitingRoom from "./screens/waiting-room";
 import { usePrivy } from "@privy-io/react-auth";
+import { Connect } from "./components/ui/connect";
 
 function App() {
   const { login, authenticated, ready } = usePrivy();
+  const [isInitialized, setIsInitialized] = useState(false);
+
+  useEffect(() => {
+    init()
+      .then(() => {
+        setIsInitialized(true);
+      })
+      .catch(() => setIsInitialized(false));
+  }, []);
+
+  if (!isInitialized) return null;
 
   if (!ready) return <p>loading...</p>;
   return (
     <main>
-      {/* <h1>Based Account Abstraction</h1> */}
-      {/* <h2>Connect and Mint your AA powered NFT now</h2> */}
-      {!authenticated && <Button onClick={login}>JOIN GAME</Button>}
-      {/* {loading && <p>Loading Smart Account...</p>} */}
-      {authenticated && <WaitingRoom />}
+      <Connect>
+        {(account, provider) => (
+          <div>
+            {/* <h1>Based Account Abstraction</h1> */}
+            {/* <h2>Connect and Mint your AA powered NFT now</h2> */}
+            {!authenticated && <Button onClick={login}>JOIN GAME</Button>}
+            {/* {loading && <p>Loading Smart Account...</p>} */}
+            {authenticated && <WaitingRoom />}
+          </div>
+        )}
+      </Connect>
     </main>
   );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const toHexString = (bytes: any) =>
+  bytes.reduce(
+    (str: any, byte: any) => str + byte.toString(16).padStart(2, "0"),
+    ""
+  );
