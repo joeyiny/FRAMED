d59b807a100b22b896b83a6c251f16a89a013ecb
joey iny <joeyainy@gmail.com>
2023-09-24 00:37:11 -0400

    Update Chat.tsx

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,84 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { Client } from '@xmtp/xmtp-js';
-import { Wallet } from 'ethers'; // Assuming you're using ethers.js
-import { Avatar } from './Avatar.tsx';
-import { Message } from './Message.tsx';
-import { MessageInput } from './MessageInput.tsx';
-import type { CachedConversation } from "@xmtp/react-sdk";
-import "@xmtp/react-components/styles.css";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 
-type Player = {
-  address: string;
-  avatarUrl?: string;
-};
-
-type ChatProps = {
-  players: Player[];
-};
-
-const Chat: React.FC<ChatProps> = ({ players }) => {
-  const [selectedPlayer, setSelectedPlayer] = useState<Player | null>(null);
-  const [messages, setMessages] = useState<any[]>([]); // Adjust the type as needed
-  const [xmtp, setXmtp] = useState<any | null>(null);
-  const [currentConversation, setCurrentConversation] = useState<CachedConversation | null>(null);
-
-
-  useEffect(() => {
-    const initializeXMTP = async () => {
-      const wallet = Wallet.createRandom(); // Replace with actual wallet
-      const client = await Client.create(wallet);
-      setXmtp(client);
-    };
-
-    initializeXMTP();
-  }, []);
-
-  const handlePlayerSelect = async (player: Player) => {
-    setSelectedPlayer(player);
-    if (xmtp) {
-      const conversation = await xmtp.conversations.newConversation(player.address);
-      setCurrentConversation(conversation);  // <-- Add this line
-      const conversationMessages = await conversation.messages();
-      setMessages(conversationMessages);
-    }
-};
-
-
-  const handleSendMessage = async (msg: string) => {
-    if (selectedPlayer && xmtp) {
-      const conversation = await xmtp.conversations.newConversation(selectedPlayer.address);
-      await conversation.send(msg);
-      const updatedMessages = await conversation.messages();
-      setMessages(updatedMessages);
-    }
-  };
-
+const Chat = () => {
   return (
-    <div>
-      <div className="player-list">
-        {players.map(player => (
-          <div key={player.address} onClick={() => handlePlayerSelect(player)}>
-            <Avatar address={player.address} url={player.avatarUrl} />
-          </div>
-        ))}
-      </div>
-
-      {selectedPlayer && (
-        <div className="chat-box">
-          <div className="messages">
-            {currentConversation && messages.map(message => (
-              <Message 
-              key={message.id} 
-              message={message} 
-              conversation={currentConversation}  // <-- Add this prop
-          />
-            ))}
-          </div>
-          <MessageInput onSubmit={handleSendMessage} placeholder="Type a message..." />
+    <div className="w-full h-full pt-[44px]">
+      <div className="w-full h-full  border-l border-slate-200">
+        <div id="chat-header" className="flex items-center">
+          <h2 className="font-semibold text-lg text-left border-b w-full pl-3 py-2">Chat</h2>
         </div>
-      )}
+        <div id="messages-area" className="text-slate-700 px-4">
+          <div className=" border-b text-left">
+            <p className="text-sm py-3">
+              <span className="font-bold">Ice Spice</span> joined the game
+            </p>
+          </div>
+        </div>
+      </div>
+      <div id="input" className="bottom-0 w-full absolute h-14 flex items-center px-2 space-x-2">
+        {/* <input type="text" className="flex-grow border rounded px-2 py-1" placeholder="Type a message..." /> */}
+        {/* <button className="ml-2 bg-blue-500 text-white rounded px-2 py-1">Send</button> */}
+        <Input placeholder="Type a message..."></Input>
+        <Button size="sm">Send</Button>
+      </div>
     </div>
   );
 };
-
 export default Chat;
