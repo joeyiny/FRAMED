a5123c0acd092d72d5033223dca6e753e0561b74
joey iny <joeyainy@gmail.com>
2023-09-23 22:01:41 -0400

    Add sticker to landing page

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -7,8 +7,10 @@ const Login = () => {
   return (
     <main className="flex items-center justify-center min-h-screen min-w-screen">
       <img src="assets/frame.png" className="w-[480px] absolute -z-20" />
-      <div className="space-y-2 flex flex-col items-center">
+      <div className="space-y-2 flex flex-col items-center relative">
         <img src="assets/logo.png" width={220} alt="FRAMED!"></img>
+        {/* the image below */}
+        <img src="assets/sticker.png" className="absolute left-32 top-36"></img>
         <TypographyH4 className="text-slate-700 text-base">in it for the art 🫡</TypographyH4>
 
         {/* <TypographyH4 className="text-slate-700 text-base">Groundbreaking encrypted blockchain game</TypographyH4> */}
