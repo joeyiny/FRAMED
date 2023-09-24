08aafdb4b3f19895d42c7cdf21e1ab7b7584d621
joey iny <joeyainy@gmail.com>
2023-09-24 00:17:42 -0400

    Add inco Chain to privy

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,26 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import * as Privy from "@privy-io/react-auth";
+import { Chain } from "@wagmi/chains";
+
+const incoChain: Chain = {
+  id: 9000,
+  name: "Inco",
+  network: "inco",
+  nativeCurrency: {
+    name: "Inco",
+    symbol: "INCO",
+    decimals: 18,
+  },
+  rpcUrls: {
+    public: {
+      http: ["https://evm-rpc.inco.network/"],
+    },
+    default: {
+      http: ["https://evm-rpc.inco.network/"],
+    },
+  },
+};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -14,6 +34,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         embeddedWallets: {
           createOnLogin: "users-without-wallets",
         },
+        additionalChains: [incoChain],
         appearance: {
           theme: "light",
           accentColor: "#FF3D00",
