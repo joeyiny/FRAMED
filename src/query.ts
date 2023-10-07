7d1435699f0ae9224e2e63a85358393104810fde
Alexandre Carvalheira <axxcar@gmail.com>
2023-10-07 18:36:49 -0300

    add new query

--- src/query.ts
+++ src/query.ts
@@ -1,13 +1,47 @@
 import { gql } from "@apollo/client";
 
-export const game = gql`
-  query Game @live(interval: 1000) {
+export const games = gql`
+  query Games @live(interval: 1000) {
     games {
       id
-      address
+      Players {
+        player {
+          id
+        }
+      }
+      roomId
+      creator
+      state
     }
+  }
+`;
+
+export const game = gql`
+  query Game($id: String!) {
+    game(id: $id) {
+      id
+      roomId
+      creator
+      state
+      Players {
+        player {
+          id
+        }
+      }
+    }
+  }
+`;
+
+export const players = gql`
+  query Players @live(interval: 1000) {
     players {
       id
+      game {
+        game {
+          id
+          roomId
+        }
+      }
     }
   }
 `;
