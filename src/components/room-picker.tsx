35cd362eb1a6a152a5a7cc634731414f761f6f80
joey iny <joeyainy@gmail.com>
2023-10-09 09:13:45 -0700

    Add basic styling to room picker

--- src/components/room-picker.tsx
+++ src/components/room-picker.tsx
@@ -1,4 +1,13 @@
 const RoomPicker = ({ games }: { games: unknown[] }) => {
-  return games.map((game, i) => <p key={i}>{game.id}</p>);
+  return (
+    <div>
+      <p className="font-bold text-lg">Pick a game:</p>
+      {games.map((game, i) => (
+        <div className="border my-2 border-zinc-600" key={i}>
+          {game.id}
+        </div>
+      ))}
+    </div>
+  );
 };
 export default RoomPicker;
