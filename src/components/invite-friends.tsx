cff09b7c8178e5a6f9a990814b95119b1679903f
Diego Aguilar <d.aguilaro99@gmail.com>
2023-10-14 17:16:40 -0500

    refactor: inviting friends

--- src/components/invite-friends.tsx
+++ src/components/invite-friends.tsx
@@ -5,15 +5,14 @@ const InviteFriends = ({ roomId }) => {
   const [copied, setCopied] = useState(false);
 
   const copyToClipboard = () => {
-    const inviteMessage = `A theft has occurred, and someone's being FRAMED! Join me in exhibit ${roomId} \nhttps://play.framed.gg`;
+    const inviteMessage = `A theft has occurred, and someone's being FRAMED! Join me in room ${roomId} \nhttps://play.framed.gg`;
     navigator.clipboard.writeText(inviteMessage);
     setCopied(true);
     setTimeout(() => setCopied(false), 2000);  // Reset the copied state after 2 seconds
   };
 
   return (
-    <div className="flex flex-col items-center w-full mt-4">
-      <h2 className="text-xl font-semibold mb-2">Invite Friends</h2>
+    <div className="flex flex-col items-center w-full mt-8">
       <Button onClick={copyToClipboard}>
         {copied ? 'Copied!' : 'Copy Invite Link'}
       </Button>
