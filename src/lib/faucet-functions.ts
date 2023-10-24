ada2df2326003dcb152e3ad25da53a2e316df362
joey iny <joeyainy@gmail.com>
2023-10-23 17:42:01 -0700

    Implement faucet function

--- src/lib/faucet-functions.ts
+++ src/lib/faucet-functions.ts
@@ -1,28 +1,21 @@
 export const fetchFundsForNewUser = async (ethereumProvider, playerAddress) => {
-  const hasFundsCache = localStorage.getItem('hasFunds');
-
-  if (hasFundsCache === 'true') {
-    console.log("User is already funded");
-    return { status: 'already_funded' };
-  }
-
-  const response = await fetch('https://faucet.inco.network/api/get-faucet', {
-    method: 'POST',
+  const response = await fetch("https://faucet.inco.network/api/get-faucet", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      address: playerAddress
-    })
+      address: playerAddress,
+    }),
   });
 
   // Check if response is successful
   if (!response.ok) {
     const responseBody = await response.json();
-    return { status: 'error', message: responseBody.message || 'Failed to fetch funds.' };
+    return { status: "error", message: responseBody.message || "Failed to fetch funds." };
   }
 
   console.log("Provided funds to ", playerAddress);
-  localStorage.setItem('hasFunds', 'true');
-  return { status: 'success' };
-}
+  localStorage.setItem("hasFunds", "true");
+  return { status: "success" };
+};
--- src/screens/authenticated.tsx
+++ src/screens/authenticated.tsx
@@ -1,11 +1,15 @@
 import Navbar from "@/components/navbar";
 
 // import TutorialFlow from "./tutorial-flow";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import InGameScreen from "./in-game";
 import { useQuery } from "@apollo/client";
 import { games } from "@/query";
 import RoomPicker from "@/components/room-picker";
+import { useWallets } from "@privy-io/react-auth";
+import { parseEther } from "ethers";
+import { fetchFundsForNewUser } from "@/lib/faucet-functions";
+// import { fetchFundsForNewUser } from "@/lib/faucet-functions";
 
 export const FACTORY_ADDRESS = "0x85F136F800130aDf5f58E15D76f671F588c623B2";
 
@@ -13,11 +17,37 @@ const Authenticated = () => {
   // const [clientState, setClientState] = useState<ClientState>(ClientState.Tutorial);
 
   // const embeddedWallet = wallets.find((wallet) => wallet.walletClientType === "privy");
+  const { wallets } = useWallets();
+  const embeddedWallet = wallets.find((wallet) => wallet.walletClientType === "privy");
 
   // const [gamePhase, setGamePhase] = useState<GamePhase>(GamePhase.WaitingForPlayers);
   const [gameContract, setGameContract] = useState<string | null>(null);
 
   const { data, loading } = useQuery(games);
+
+  useEffect(() => {
+    if (!embeddedWallet) return;
+    const provideInitialFunds = async () => {
+      const balance = await (await embeddedWallet.getEthersProvider()).getBalance(embeddedWallet.address);
+      const halfETHinWei = parseEther("0.5");
+      // console.log(balance);
+      if (balance.lte(halfETHinWei)) {
+        // console.log("ur balance is less than 0.5. ur balance: ", balance);
+        const result = await fetchFundsForNewUser(embeddedWallet.getEthersProvider(), embeddedWallet.address);
+        if (result.status === "success") {
+          // setHasFunds(true);
+        } else if (result.status === "error") {
+          console.error("Error fetching funds:", result.message);
+        } else {
+          console.log(
+            result.status === "already_funded" ? "User is already funded" : "Unexpected status:",
+            result.status
+          );
+        }
+      }
+    };
+    provideInitialFunds().catch((error) => console.error("Unexpected error:", error));
+  }, [embeddedWallet]);
 
   if (loading || !data) return <p>loading</p>;
   return (
